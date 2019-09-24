d2bdf2fa819f0486cd78df0873a0c83586d033c8
Kevin Engström <kevin.engstrom@hotmail.com>
2019-09-24 09:38:48 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -7,6 +7,28 @@ namespace TODO
         static void Main(string[] args)
         {
 
+            bool willExit = false;
+
+            while (!willExit)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Add ToDo");
+                Console.WriteLine("2. List ToDo");
+                Console.WriteLine("3. Exit");
+
+                ConsoleKeyInfo input = Console.ReadKey(true);
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+                        break;
+                    case ConsoleKey.D2:
+                        break;
+                    case ConsoleKey.D3:
+                        willExit = true;
+                        break;
+                }
+            }
         }
     }
 }
